16d510e190f5f6183f844efc85ab1887d06a5e11
Kelly Taylor <kelly.taylor@kibocommerce.com>
2024-07-05 14:30:17 -0700

    delete tests

--- src/test/java/com/kibocommerce/bpm/fulfillment/TestHelper.kt
+++
@@ -1,8 +0,0 @@
-package com.kibocommerce.bpm.fulfillment
-
-import org.kie.api.runtime.process.WorkflowProcessInstance
-import kotlin.test.assertEquals
-
-fun assertCurrentState(wpi: WorkflowProcessInstance, expectedState: String) {
-    assertEquals(expectedState, wpi.getVariable("currentState"))
-}
